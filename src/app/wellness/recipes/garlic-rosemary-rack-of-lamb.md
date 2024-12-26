547e35f9e63977015f5129efc919e1e4c2d98bc2
ali <aliismaildeveloper@gmail.com>
2024-12-25 21:25:06 -0800

    feat: guides has been added to recipes

---
+++ src/app/wellness/recipes/garlic-rosemary-rack-of-lamb.md
@@ -0,0 +1,35 @@
+## Garlic Rosemary Rack of Lamb
+
+### **Tools**
+
+- Cast iron pan/oven
+- Cutting board
+- Knife
+- Small bowl to make sauce
+- Oven
+
+### **Ingredients**
+
+- Rack of lamb
+- olive oil
+- salt and pepper
+- turmeric
+- garam masala
+- garlic
+- rosemary
+- unsalted butter
+
+### **Steps**
+
+```
+- Open Rack of lamb
+- Add olive oil
+- Add salt and pepper
+- Add turmeric and salt and pepper
+- sear in cast iron on all sides
+- chop fresh garlic and rosemary
+- add unsalted butter to garlic rosemary chop
+- Adhere garlic butter to rack of lamb (make a crust)
+- Cook at 300 until internal temp of 140 (about 35min)
+- Rest for 15 min
+```
--- src/app/wellness/recipes/page.tsx
+++ src/app/wellness/recipes/page.tsx
@@ -1,3 +1,34 @@
-export default function RecipesPage() {
-    return <div>Welcome to the Recipes page!</div>;
+// src/app/wellness/recipes/page.tsx
+import path from 'path';
+import Link from 'next/link';
+import { getGuides } from '@/utils/getGuides';
+import { Guide } from '@/types/guide';
+
+export default async function RecipesPage() {
+
+    const dir = path.join(process.cwd(), 'src/app/wellness/recipes');
+
+    let guides: Guide[] = [];
+    try {
+        guides = await getGuides(dir);
+    }
+    catch (error) {
+        console.error("Error fetching guides: ", error);
+    }
+
+    return (
+        <div className="p-6">
+            <h1 className="text-3xl font-bold mb-4">Recipes</h1>
+            <ul className="space-y-4">
+                {guides.map((guide) => (
+                    <li key={guide.slug}>
+                        <Link href={`/islam/${guide.slug}`}>
+                            {guide.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+    );
 }
