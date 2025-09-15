a440ca05f2c844e6e98d9bb657b7220d3fcaf875
Ali Ismail <aliismaildeveloper@gmail.com>
2025-09-14 22:30:48 -0700

    refactor(layout): extract Footer and centralize social links config

---
+++ src/lib/site.ts
@@ -0,0 +1,8 @@
+export const site = {
+  name: 'Mentalchai',
+  links: {
+    github: 'https://github.com/ali-ismail-1',
+    linkedin: 'https://www.linkedin.com/in/ali-ismail-35196615/',
+    x: 'https://x.com/Ali_F_Ismail',
+  },
+};
